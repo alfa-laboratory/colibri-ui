742fae3c7f3a99d2bbb74731ea355d153c857b29
Александр Долинский <adolinskiy@alfabank.ru>
2017-06-22 09:55:47 +0300

    add Map

--- src/main/java/ru/colibri/ui/core/contexts/TestContext.java
+++ src/main/java/ru/colibri/ui/core/contexts/TestContext.java
@@ -4,9 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 @Getter
 @Setter
 public class TestContext {
     private String currentPageName;
+    private Map<String, String> context = new HashMap<>();
 }
