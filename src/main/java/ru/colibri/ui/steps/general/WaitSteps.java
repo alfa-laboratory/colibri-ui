4f465d3f19c338dbfbc916ffe10143d603227f4b
Александр Долинский <xaker6662007@gmail.com>
2017-06-23 16:56:47 +0300

    добавлено действие "подождать"

---
+++ src/main/java/ru/colibri/ui/steps/general/WaitSteps.java
@@ -0,0 +1,16 @@
+package ru.colibri.ui.steps.general;
+
+import org.jbehave.core.annotations.When;
+import org.springframework.stereotype.Component;
+import ru.colibri.ui.core.steps.AbsSteps;
+import ru.yandex.qatools.allure.annotations.Step;
+
+@Component
+public class WaitSteps extends AbsSteps {
+
+    @Step
+    @When("ожидаем $timeInterval {секунд|секунду|секунды}")
+    public void waitSomeTime(int timeInterval) throws InterruptedException {
+        Thread.sleep(timeInterval * 1000);
+    }
+}
