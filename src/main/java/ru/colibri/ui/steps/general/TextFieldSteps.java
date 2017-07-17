7b695cf690222fda2b0d3ee846ea7dfd6ea23c13
Александр Долинский <adolinskiy@alfabank.ru>
2017-07-17 10:35:06 +0300

    Добавлен метод по очистке полей. Изменен sendKeys на setValue

--- src/main/java/ru/colibri/ui/steps/general/TextFieldSteps.java
+++ src/main/java/ru/colibri/ui/steps/general/TextFieldSteps.java
@@ -1,8 +1,11 @@
 package ru.colibri.ui.steps.general;
 
+import io.appium.java_client.MobileElement;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.When;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Keyboard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.colibri.ui.core.steps.AbsSteps;
@@ -19,7 +22,7 @@ public class TextFieldSteps extends AbsSteps {
     public void sendKeys(@Named("$field") String field, @Named("$valueOrKeyword") String valueOrKeyword) {
         WebElement webElement = getWebElementByName(field);
         String value = propertyUtils.injectProperties(valueOrKeyword);
-        webElement.sendKeys(value);
+        ((MobileElement) webElement).setValue(value);
     }
 
 
@@ -38,5 +41,15 @@ public class TextFieldSteps extends AbsSteps {
     public void sendText(@Named("$textOrKeyword") String textOrKeyword) {
         String text = propertyUtils.injectProperties(textOrKeyword);
         driver.getKeyboard().sendKeys(text);
+    }
+
+    @Step
+    @When("очистить \"$fieldName\"")
+    public void clearField(@Named("$fieldName") String fieldName) {
+        WebElement webElement = getWebElementByName(fieldName);
+        Keyboard keyboard = driver.getKeyboard();
+        for (int i = webElement.getText().length(); i > 0; i--) {
+            keyboard.sendKeys(Keys.BACK_SPACE);
+        }
     }
 }
