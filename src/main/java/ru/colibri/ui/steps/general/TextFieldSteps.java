9b84e676e91ca200f3b83b790857e9a293892f83
dimaprokop <adrenalin11@mail.ru>
2017-08-29 11:43:04 +0300

    revert clearField method to loop strategy

--- src/main/java/ru/colibri/ui/steps/general/TextFieldSteps.java
+++ src/main/java/ru/colibri/ui/steps/general/TextFieldSteps.java
@@ -3,7 +3,9 @@ package ru.colibri.ui.steps.general;
 import io.appium.java_client.MobileElement;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.When;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Keyboard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.colibri.ui.core.steps.AbsSteps;
@@ -41,10 +43,12 @@ public class TextFieldSteps extends AbsSteps {
         driver.getKeyboard().sendKeys(text);
     }
 
-    @Step
     @When("очистить \"$fieldName\"")
     public void clearField(@Named("$fieldName") String fieldName) {
         WebElement webElement = getWebElementByName(fieldName);
-        webElement.clear();
+        Keyboard keyboard = driver.getKeyboard();
+        for (int i = webElement.getText().length(); i > 0; i--) {
+            keyboard.sendKeys(Keys.BACK_SPACE);
+        }
     }
 }
