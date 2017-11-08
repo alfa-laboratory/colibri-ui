54dba2b10e8e459a678724f97bad6575cc8b18cd
Александр Долинский <xaker6662007@gmail.com>
2017-11-08 14:08:23 +0300

    add CheckSteps

--- src/main/java/ru/colibri/ui/steps/general/CheckSteps.java
+++ src/main/java/ru/colibri/ui/steps/general/CheckSteps.java
@@ -1,5 +1,7 @@
 package ru.colibri.ui.steps.general;
 
+import org.hamcrest.CoreMatchers;
+import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +11,11 @@ import ru.colibri.ui.core.steps.AbsSteps;
 import ru.colibri.ui.settings.general.PropertyUtils;
 import ru.yandex.qatools.allure.annotations.Step;
 
+import java.util.List;
+
 import static java.lang.String.format;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.*;
 
 @Component
 public class CheckSteps extends AbsSteps {
@@ -45,4 +49,31 @@ public class CheckSteps extends AbsSteps {
         increaseImplicitlyWait();
         assertEquals("Найден элемент на странице", 0, sizeOfList);
     }
+
+    @Step
+    @Then("каждый элемент \"$elementName\" содержит значение \"$template\" без учета регистра")
+    public void eachElementContainsValue(@Named("$elementName") String elementName, @Named("$template") String template) {
+        IElement element = getCurrentPage().getElementByName(elementName);
+        List<WebElement> elementsFound = finder.findWebElements(element);
+        String expectedValue = propertyUtils.injectProperties(template);
+        elementsFound.forEach(elem -> {
+            assertThat("Элемент не содержит заданное значение", elem.getText().toLowerCase(), containsString(expectedValue.toLowerCase()));
+        });
+    }
+
+    @Step
+    @Then("каждый элемент \"$elementName\" содержит любое из значений \"$template1\" или \"$template2\" без учета регистра")
+    public void eachElementContainsOneOfTwoValues(@Named("$elementName") String elementName, @Named("$template1") String templateOne, @Named("$template2") String templateTwo) {
+        IElement element = getCurrentPage().getElementByName(elementName);
+        List<WebElement> elementsFound = finder.findWebElements(element);
+        String expectedValueOne = propertyUtils.injectProperties(templateOne);
+        String expectedValueTwo = propertyUtils.injectProperties(templateTwo);
+        elementsFound.forEach(elem -> {
+            assertThat("Элемент не содержит ни одно из заданных значений", elem.getText().toLowerCase(), CoreMatchers.anyOf(
+                    containsString(expectedValueOne.toLowerCase()),
+                    containsString(expectedValueTwo.toLowerCase())
+            ));
+        });
+    }
+
 }
