c64926b56101dd51e4308ef8f3f7d264e0054b7a
Aleksandr Dolinskiy <xaker6662007@gmail.com>
2017-11-16 18:57:27 +0300

    Add button steps (#30)
    * Add button steps
    
    * Add button steps

--- src/main/java/ru/colibri/ui/steps/general/WaitSteps.java
+++ src/main/java/ru/colibri/ui/steps/general/WaitSteps.java
@@ -1,6 +1,7 @@
 package ru.colibri.ui.steps.general;
 
 import org.jbehave.core.annotations.Alias;
+import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.When;
 import org.springframework.stereotype.Component;
 import ru.colibri.ui.core.steps.AbsSteps;
@@ -11,8 +12,8 @@ public class WaitSteps extends AbsSteps {
 
     @Step
     @When("ожидаем $timeInterval {секунд|секунду|секунды}")
-    @Alias("подождать timeInterval {секунд|секунду|секунды}")
-    public void waitSomeTime(int timeInterval) throws InterruptedException {
+    @Alias("подождать $timeInterval {секунд|секунду|секунды}")
+    public void waitSomeTime(@Named("$timeInterval") int timeInterval) throws InterruptedException {
         Thread.sleep(timeInterval * 1000);
     }
 }
