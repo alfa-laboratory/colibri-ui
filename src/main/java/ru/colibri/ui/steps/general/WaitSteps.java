d259f4681e97939639e8d45ce1ab072a8febd4e1
adolinskiy <xaker6662007@gmail.com>
2017-11-21 17:54:43 +0300

    add Aliases

--- src/main/java/ru/colibri/ui/steps/general/WaitSteps.java
+++ src/main/java/ru/colibri/ui/steps/general/WaitSteps.java
@@ -1,6 +1,6 @@
 package ru.colibri.ui.steps.general;
 
-import org.jbehave.core.annotations.Alias;
+import org.jbehave.core.annotations.Aliases;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.When;
 import org.springframework.stereotype.Component;
@@ -12,7 +12,8 @@ public class WaitSteps extends AbsSteps {
 
     @Step
     @When("ожидаем $timeInterval {секунд|секунду|секунды}")
-    @Alias("подождать $timeInterval {секунд|секунду|секунды}")
+    @Aliases(values = {"подождать $timeInterval {секунд|секунду|секунды}",
+                        "подождать \"$timeInterval\" {секунд|секунду|секунды}"})
     public void waitSomeTime(@Named("$timeInterval") int timeInterval) throws InterruptedException {
         Thread.sleep(timeInterval * 1000);
     }
