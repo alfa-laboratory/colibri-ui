0428f6a95f019fe0bc1425dc8c1f1949939c8220
Prokop Dmitry <18467259+JasperJhons@users.noreply.github.com>
2018-03-15 12:14:51 +0300

    #extract report portal from core (#44)
    * #extract report portal from core

--- src/main/java/ru/colibri/ui/core/settings/IJBConfigurator.java
+++ src/main/java/ru/colibri/ui/core/settings/IJBConfigurator.java
@@ -3,11 +3,13 @@ package ru.colibri.ui.core.settings;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.embedder.EmbedderControls;
 import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.ViewGenerator;
 
 public interface IJBConfigurator {
+
     Configuration createConfig();
 
-    Configuration createConfig(Format... formats);
+    Configuration createConfig(ViewGenerator viewGenerator, Format... formats);
 
     void configure(EmbedderControls embedderControls, DriversSettings driversSettings);
 }
